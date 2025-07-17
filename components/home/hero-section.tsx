30f15900bef62f2a74a977fe7b1f77bb3240e5b9
bagoesrex <bagoesrex@gmail.com>
2025-07-17 20:57:04 +0700

    feat(hero): add Megumin avatar and adjust layout

--- components/home/hero-section.tsx
+++ components/home/hero-section.tsx
@@ -1,13 +1,15 @@
 import BackgroundImage from "../shared/background-image";
 import SplitTextCustom from "../shared/split-text-custom";
 import BlurTextCustom from "../shared/blur-text-custom";
+import MeguminAvatar from "../shared/megumin-avatar";
 
 export default function HeroSection() {
     return (
         <section id="hero" className="relative bg-cover bg-center min-h-screen px-5">
             <BackgroundImage />
+            <MeguminAvatar />
 
-            <div className="max-w-5xl mx-auto w-full py-10 flex flex-col gap-2 items-start">
+            <div className="max-w-5xl mx-auto w-full pt-10 flex flex-col gap-2 items-start min-h-svh">
                 <SplitTextCustom text="MEGUMINE" />
 
                 <BlurTextCustom text="Megumine is My Istri" showAfter={400} />
---
+++ components/shared/megumin-avatar.tsx
@@ -0,0 +1,17 @@
+type Props = {
+    imageUrl?: string;
+};
+
+export default function MeguminAvatar({
+    imageUrl = "/images/avatars/megumin-avatar.webp"
+}: Props) {
+    return (
+        <div
+            className="absolute bottom-0 right-0 bg-cover bg-no-repeat w-[589px] h-[421px]"
+            style={{
+                backgroundImage: `url(${imageUrl})`,
+            }}
+        />
+    );
+}
+
