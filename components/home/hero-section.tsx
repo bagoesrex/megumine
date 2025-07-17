fdc7e2c9826239c43e5d91db9b8714272e6f8ebc
bagoesrex <bagoesrex@gmail.com>
2025-07-17 19:58:25 +0700

    feat(hero): enhance hero section with updated BlurText and SplitText components

--- components/home/hero-section.tsx
+++ components/home/hero-section.tsx
@@ -1,21 +1,16 @@
-"use client"
-
 import BackgroundImage from "../shared/background-image";
-import SplitText from "../react-bits/TextAnimations/SplitText/SplitText";
+import SplitTextCustom from "../shared/split-text-custom";
+import BlurTextCustom from "../shared/blur-text-custom";
 
 export default function HeroSection() {
     return (
         <section id="hero" className="relative bg-cover bg-center min-h-screen px-5">
             <BackgroundImage />
 
-            <div className="max-w-5xl mx-auto py-10">
-                <SplitText
-                    text="MEGUMINE"
-                    className="text-5xl text-primary text-center font-extrabold overflow-visible text-shadow-md text-shadow-white"
-                    ease="elastic.out"
-                    splitType="chars"
-                    duration={1.5}
-                />
+            <div className="max-w-5xl mx-auto w-full py-10 flex flex-col gap-2 items-start">
+                <SplitTextCustom text="MEGUMINE" />
+
+                <BlurTextCustom text="Megumine is My Istri" showAfter={400} />
             </div>
         </section>
     )
