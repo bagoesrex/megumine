68634fa0c699532da25e557bf00b1dd93865edb9
bagoesrex <bagoesrex@gmail.com>
2025-07-19 19:59:36 +0700

    feat(story): add scroll-in animation to story section

--- components/home/story-section.tsx
+++ components/home/story-section.tsx
@@ -8,7 +8,7 @@ export default function StorySection() {
         <section id="story" className="relative bg-cover bg-center min-h-screen px-5 overflow-hidden">
             <BackgroundImage imageUrl="/images/bg/megumine-bg.webp" />
 
-            <div className="max-w-5xl mx-auto w-full py-10 flex flex-col gap-10 items-start min-h-svh">
+            <div className="max-w-5xl mx-auto w-full py-20 flex flex-col gap-20 items-start min-h-svh">
                 {storyData.map((story, index) => (
                     <StoryContent key={index}
                         title={story.title}
--- components/shared/story-content.tsx
+++ components/shared/story-content.tsx
@@ -1,36 +1,61 @@
-import BlurText from "../react-bits/TextAnimations/BlurText/BlurText"
-import BlurTextCustom from "./blur-text-custom"
-import SplitTextCustom from "./split-text-custom"
-import StoryCard from "./story-card"
+"use client";
+
+import { useInView } from "react-intersection-observer";
+import BlurText from "../react-bits/TextAnimations/BlurText/BlurText";
+import BlurTextCustom from "./blur-text-custom";
+import SplitTextCustom from "./split-text-custom";
+import StoryCard from "./story-card";
+import { motion } from "framer-motion";
 
 type Props = {
-    title: string,
-    subtitle: string,
-    description: string,
-    index: number,
-    imageUrl: string,
-}
+    title: string;
+    subtitle: string;
+    description: string;
+    index: number;
+    imageUrl: string;
+};
 
-export default function StoryContent({ title, subtitle, description, index, imageUrl }: Props) {
+export default function StoryContent({
+    title,
+    subtitle,
+    description,
+    index,
+    imageUrl,
+}: Props) {
+    const { ref, inView } = useInView({
+        triggerOnce: true,
+        threshold: 0.8,
+    });
+
     return (
-        <div className="w-full">
-            <SplitTextCustom text={title} duration={1.3} />
-            <BlurText
-                text={subtitle}
-                delay={10}
-                animateBy="letters"
-                direction="bottom"
-                className="text-1xl leading-relaxed mb-2"
-            />
+        <div ref={ref} className="w-full">
+            <motion.div
+                initial={{ opacity: 0, y: 40 }}
+                animate={inView ? { opacity: 1, y: 0 } : {}}
+                transition={{ duration: 0.7, ease: "easeOut" }}
+                className="transition-all"
+            >
+                <SplitTextCustom text={title} duration={1.3} />
+                <BlurText
+                    text={subtitle}
+                    delay={10}
+                    animateBy="letters"
+                    direction="bottom"
+                    className="text-1xl leading-relaxed mb-2"
+                />
 
-            <div className={`flex w-full gap-4 ${index % 2 !== 0 ? 'flex-row-reverse' : 'flex-row'}`}>
-                <div className="max-w-180 w-full">
-                    <BlurTextCustom text={description} showAfter={400} />
+                <div
+                    className={`flex w-full gap-4 ${index % 2 !== 0 ? "flex-row-reverse" : "flex-row"
+                        }`}
+                >
+                    <div className="max-w-180 w-full">
+                        <BlurTextCustom text={description} showAfter={400} />
+                    </div>
+                    <div className="max-w-150 flex flex-col gap-4">
+                        <StoryCard imageUrl={imageUrl} />
+                    </div>
                 </div>
-                <div className="max-w-150 flex flex-col gap-4">
-                    <StoryCard imageUrl={imageUrl} />
-                </div>
-            </div>
+            </motion.div>
         </div>
-    )
+    );
 }
