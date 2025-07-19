c6a086aa667fa63945ce509112da8ca549d33c87
bagoesrex <bagoesrex@gmail.com>
2025-07-19 19:42:05 +0700

    feat(animation): trigger explosion animation on scroll into view

--- components/shared/explosion-effect-wrapper.tsx
+++ components/shared/explosion-effect-wrapper.tsx
@@ -1,12 +1,24 @@
-"use client"
+"use client";
 
-import ExplosionLottie from "../animations/explosion-lottie"
+import ExplosionLottie from "../animations/explosion-lottie";
+import { useInView } from "react-intersection-observer";
 
 export default function ExplosionEffectWrapper() {
+    const { ref, inView } = useInView({
+        triggerOnce: true,
+        threshold: 0.2,
+    });
+
     return (
-        <div className="absolute -bottom-55 -right-80 -z-10">
-            <ExplosionLottie />
-        </div>
-    )
+        <>
+            {inView && (
+                <div className="absolute -bottom-55 -right-90 -z-10">
+                    <ExplosionLottie />
+                </div>
+            )}
+
+            <div ref={ref} className="h-[1px]" />
+        </>
+    );
 }
 
